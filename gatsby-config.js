db2bf00fa60558b61927d3778a992cb617fe73ab
minung.han <hmu332233@gmail.com>
2021-01-16 16:40:55 +0000

    build: Add tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,41 @@
 module.exports = {
   siteMetadata: {
-    title: "help-jbnu--page",
+    title: 'help-jbnu--page',
   },
   plugins: [
-    "gatsby-plugin-sass",
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [
+          require('tailwindcss')('./configs/tailwind.config.js'),
+          require('autoprefixer'),
+        ],
+      },
+    },
     // {
     //   resolve: "gatsby-plugin-google-analytics",
     //   options: {
     //     trackingId: "",
     //   },
     // },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-transformer-sharp",
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
