b4b055332f90a827388f32f6329afc1495d13082
minung.han <hmu332233@gmail.com>
2021-01-29 03:28:05 +0900

    feat: graphql node로 store 데이터 추가

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,37 @@
 const STORES = [
-  'cham',
-  'basic',
-  'characterization',
-  'jinsu',
-  'medi',
-  'hu',
-  'jungdam',
+  { name: '참빛관', value: 'cham' },
+  { name: '직영관', value: 'basic' },
+  { name: '특성화 캠퍼스', value: 'characterization' },
+  { name: '진수당', value: 'jinsu' },
+  { name: '의대', value: 'medi' },
+  { name: '후생관', value: 'hu' },
+  { name: '정담원', value: 'jungdam' },
 ];
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode } = actions;
+
+  STORES.forEach((store) => {
+    console.log(store);
+    createNode({
+      id: createNodeId(`store-${store.value}`),
+      name: store.name,
+      value: store.value,
+      internal: {
+        type: 'Store',
+        contentDigest: createContentDigest(store),
+      },
+    });
+  });
+};
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
 
-  // category page
+  // store page
   STORES.forEach((store) => {
-    console.log(store);
     createPage({
-      path: `/stores/${store}`,
+      path: `/stores/${store.value}`,
       component: require.resolve(`./src/pages/index.js`),
       context: {
         store,
