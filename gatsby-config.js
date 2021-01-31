a7eb4dbfdf598ee1caed23bf4efddbe8c53de853
minung.han <hmu332233@gmail.com>
2021-01-31 22:08:53 +0900

    feat: GA 설정 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,12 +12,15 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: "gatsby-plugin-google-analytics",
-    //   options: {
-    //     trackingId: "",
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-PYVBHNES4P', // Google Analytics / GA
+        ],
+      },
+    },
     'gatsby-plugin-sharp',
     'gatsby-plugin-react-helmet',
     // 'gatsby-plugin-sitemap',
