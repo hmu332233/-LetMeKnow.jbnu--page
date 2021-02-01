bea26e033f78f693de19a4231618a965f1e3860b
minung.han <hmu332233@gmail.com>
2021-02-02 01:06:42 +0900

    feat: meta 데이터 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Layout from '@components/Layout';
 import StoreSelect from '@containers/StoreSelect';
 import StoreMenuList from '@containers/StoreMenuList';
+import SEO from '@containers/Seo';
 
 // markup
 const IndexPage = (props) => {
-  const store = props.pageContext.store || { value: 'cham' };
+  const store = props.pageContext.store || { name: '참빛관', value: 'cham' };
   return (
     <Layout>
+      <SEO title={`${store.name} 메뉴`} />
       <StoreSelect value={store.value} />
       <StoreMenuList value={store.value} />
     </Layout>
